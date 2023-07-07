5284177735c7f80d20a84d9ee2a8369ae23bb4b3
Obed Paz <62860279+lesterpaz99@users.noreply.github.com>
2023-07-07 11:10:45 -0600

    Fix first quote empty array

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ import './App.css'
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [currentQuote, setCurrentQuote] = useState(null);
+  const [color, setColor] = useState('black');
 
   // logic here ...
   useEffect(() => {
-    // const controller = new AbortController();
+    const controller = new AbortController();
 
     const fetchQuotes = async () => {
       try {
         const response = await axios.get('https://type.fit/api/quotes', {
-        // signal: controller.signal
+        signal: controller.signal
       });
       setQuotes(response.data);
+
+      // set initial quote
+      const randomNumber = Math.floor(Math.random() * response.data.length);
+      setCurrentQuote(response.data[randomNumber]);
+
 
     } catch (err) {
         console.log(err);
@@ -25,21 +31,33 @@ function App() {
 
     fetchQuotes();
 
-    // set initial quote
-    getNewQuote();
-    // return () => {
-    //   controller.abort();
-    // };
+    return () => {
+      controller.abort();
+    };
   }, []);
+
 
   const getNewQuote = () => {
     const randomNumber = Math.floor(Math.random() * quotes.length);
     setCurrentQuote(quotes[randomNumber]);
+
+    // generate a random color string
+    const randomColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+    setColor(randomColor);
   }
 
-  if (quotes.length === 0 && currentQuote === null) return <h1>Loading...</h1>;
+  // function getRandomColor() {
+  //   const letters = '0123456789ABCDEF';
+  //   let color = '#';
+  //   for (let i = 0; i < 6; i++) {
+  //     color += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return color;
+  // }
 
-  return <Quote currentQuote={currentQuote} getNewQuote={getNewQuote} />;
+  if (!currentQuote) return <h1>Loading...</h1>;
+
+  return <Quote currentQuote={currentQuote} getNewQuote={getNewQuote} color={color} />;
 }
 
 export default App
