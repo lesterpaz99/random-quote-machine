a6088155726a54f4c4c59e588bf7099b042a07be
Obed Paz <62860279+lesterpaz99@users.noreply.github.com>
2023-07-07 11:11:12 -0600

    Add color and bgColor to components when new quote

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -4,24 +4,27 @@ import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import { Twitter, Instagram } from '@mui/icons-material';
 
 
-export const Quote = ({ currentQuote, getNewQuote }) => {
+export const Quote = ({ currentQuote, getNewQuote, color }) => {
+
+  document.getElementById('body').style.backgroundColor = color;
+
   return (
-    <div className='cardContainer'>
+    <div className='cardContainer' style={{ color }}>
       <div className='quote'>
         <h1><span><FormatQuoteIcon style={{rotate: '180deg'}} /></span>{' '}{currentQuote?.text}</h1>
         <p className='author'>- {currentQuote?.author}</p>
       </div>
       <div className='buttons'>
         <div className='socialButtons'>
-          <Button variant='contained' href=''>
+          <Button variant='contained' href='' style={{ backgroundColor: color }}>
             <Twitter />
           </Button>
-          <Button variant='contained' href=''>
+          <Button variant='contained' href='' style={{ backgroundColor: color }}>
             <Instagram />
           </Button>
         </div>
         <div>
-          <Button onClick={getNewQuote} variant='contained' style={{textTransform: 'capitalize'}}>
+          <Button onClick={getNewQuote} variant='contained' style={{textTransform: 'capitalize', backgroundColor: color}}>
             New Quote
           </Button>
         </div>
@@ -35,5 +38,6 @@ Quote.propTypes = {
     text: PropTypes.string,
     author: PropTypes.string
   }),
-  getNewQuote: PropTypes.func.isRequired
+  getNewQuote: PropTypes.func.isRequired,
+  color: PropTypes.string
 }
