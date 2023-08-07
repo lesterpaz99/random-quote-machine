51237d56100b5297538ec52e3b853601a3ceea4e
Obed Paz <lesterpaz99@gmail.com>
2023-08-06 23:17:28 -0600

    Add id attrb to pass test cases in user stories

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -10,18 +10,18 @@ export const Quote = ({ currentQuote, getNewQuote, color }) => {
   }
 
   return (
-    <div className='cardContainer' style={{ color }}>
+    <div className='cardContainer' style={{ color }} id='quote-box'>
       <div className='quote'>
-        <h1>
+        <h1 id='text'>
           <FormatQuoteIcon style={{rotate: '180deg'}} alt='quote icon' />
           {' '}
           {currentQuote.text}
         </h1>
-        <p className='author'>- {currentQuote.author}</p>
+        <p className='author' id='author'>- {currentQuote.author}</p>
       </div>
       <div className='buttons'>
         <div className='socialButtons'>
-          <Button variant='contained' target='_blank' href={`https://twitter.com/intent/tweet?text=${currentQuote.text}`} style={{ backgroundColor: color }}>
+          <Button id='tweet-quote' variant='contained' rel="noreferrer" target='_blank' href={`https://twitter.com/intent/tweet?text=${currentQuote.text}`} style={{ backgroundColor: color }}>
             <Twitter alt='twitter icon' />
           </Button>
           <Button variant='contained' href='' style={{ backgroundColor: color }}> {/* Insert Instagram sharing link */}
@@ -29,7 +29,7 @@ export const Quote = ({ currentQuote, getNewQuote, color }) => {
           </Button>
         </div>
         <div>
-          <Button onClick={getNewQuote} variant='contained' style={{textTransform: 'capitalize', backgroundColor: color}}>
+          <Button onClick={getNewQuote} variant='contained' style={{textTransform: 'capitalize', backgroundColor: color}} id='new-quote'>
             New Quote
           </Button>
         </div>
